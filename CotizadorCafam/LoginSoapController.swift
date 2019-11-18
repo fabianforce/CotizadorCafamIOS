a206f96873ff627db0871417e95bcc4848035fe5
Fabian Castillo <fabiancasti_94@hotmail.com>
2019-11-18 08:29:56 -0500

    first commit

---
+++ CotizadorCafam/LoginSoapController.swift
@@ -0,0 +1,9 @@
+//
+//  LoginSoapController.swift
+//  CotizadorCafam
+//
+//  Created by Fabian Humberto Castillo Pineda on 18/11/19.
+//  Copyright © 2019 Fabian Humberto Castillo Pineda. All rights reserved.
+//
+
+import Foundation
