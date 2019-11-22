d697b03ce6c76c1d4053b63fb1c9246552bf4dc2
Fabian Castillo <fabiancasti_94@hotmail.com>
2019-11-22 16:59:20 -0500

    service menu

---
+++ CotizadorCafam/ServiceMenuViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ServiceMenuViewController.swift
+//  CotizadorCafam
+//
+//  Created by Fabian Humberto Castillo Pineda on 22/11/19.
+//  Copyright © 2019 Fabian Humberto Castillo Pineda. All rights reserved.
+//
+
+import UIKit
+
+class ServiceMenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
